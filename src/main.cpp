85ec13a820aa3e0ccebf2b8fb1867f05d7991acc
Mustahid <mustahid34@gmail.com>
2022-07-20 17:44:58 +0600

    Graphics

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Day 01
+	/*Display Graphics*/
+
+	cout << "			T I C K  T O C   T A C		" << endl
+		 << endl;
+	cout << " =>  Player 1[X] <=" << endl
+		 << " 	VS  " << endl
+		 << "=>   Player 2[O] <= " << endl;
+	cout << endl;
+
+	// Displaying Board
+
+	cout << "     |     |     " << endl;
+	cout << "     |     |     " << endl;
+	cout << "_____|_____|_____" << endl;
+	cout << "     |     |     " << endl;
+	cout << "     |     |     " << endl;
+	cout << "_____|_____|_____" << endl;
+	cout << "     |     |     " << endl;
+	cout << "     |     |     " << endl;
+	cout << endl;
+}
